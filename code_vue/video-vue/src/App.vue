501f313a196dfa57c8f1577a0aeab191c652f6df
jsq <1647013149@qq.com>
2019-02-20 17:50:34 +0800

    133

---
+++ code_vue/video-vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang=”less”>   /*scoped的作用仅仅是限定css的作用域，防止变量污染*/
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ code_vue/video-vue/src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="index">
+    <div class="login">
+      123
+    </div>
+    <el-input>用户名</el-input>
+    <el-input>密码</el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+ .index{
+   width: 100%;
+   height: 100%;
+   position: absolute;
+   background: url('~@/resource/index.jpg')  no-repeat;
+   background-size: contain;
+ }
+</style>
